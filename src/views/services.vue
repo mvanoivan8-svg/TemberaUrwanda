6683d4540a5c4df3e66405083dd00765ade6e262
Mvano Ivan <mvanoivan8@gmail.com>
2026-05-29 12:22:48 +0200

    final

---
+++ src/views/services.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h1 class="text-5xl text-green-800 text-center mt-20  font-bold ">Services we provide</h1>
+    <p class="text-xl text-center mt-10 text-2xl mb-10">We offer more services in this services website, this website is originally designed <br>
+      for more locations for places to visit in rwanda either in northern, eastern, <br
+      >western, southern and kigali city Lorem ipsum dolor sit amet consectetur <br> 
+      adipisicing elit. Possimus obcaecati  dolorem optio, quia quaerat <br>
+      autem distinctio id iusto incidunt necessitatibus placeat tenetur <br>
+       aut blanditiis voluptas sit eius. Ipsam, facere sed! </p>
+
+  </section>
+</template>
